e8b1d0c8d6d6c491f686ed29cb00a4411fae3879
aurora <dragoiaurora92@gmail.com>
2020-02-20 22:02:04 +0200

    import person project

---
+++ ClassAndObjectPerson/Persoana.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndObjectPerson
+{
+    class Persoana
+    {
+        public string nume { get; set; }
+        public string prenume { get; set; }
+        public int varsta { get; set; }
+
+        // constructor implicit
+        public Persoana()
+        {
+            nume = "";
+            prenume = "";
+            varsta = 0;
+        }
+
+        //constructor cu 1 parametru
+
+        public Persoana(string nume)
+        {
+            this.nume = nume;
+            this.prenume = "";
+            this.varsta = 0;
+        }
+
+        public Persoana(string name, string firstName, int age)
+        {
+            this.nume = name;
+            this.prenume = firstName;
+            this.varsta = age;
+        }
+
+        public string afiseazaDetaliiPersoana()
+        {
+            return "Nume: " + nume + "\nPrenume: " + prenume + "\nVarsta: " + varsta;
+        }
+    }
+}
---
+++ ClassAndObjectPerson/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassAndObjectPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persoana pers = new Persoana();
+            pers.nume = "Dragoi";
+            pers.prenume = "Aurora";
+            pers.varsta = 28;
+            Console.WriteLine(pers.afiseazaDetaliiPersoana());
+
+            /*Console.WriteLine();
+            Console.WriteLine("Nume: " + pers.nume + "\nPrenume: " + pers.prenume + "\nVarsta: " + pers.varsta);*/
+
+            Console.WriteLine();
+            Console.WriteLine("Nume: " + pers.nume);
+
+            Persoana pers1 = new Persoana("Ionescu");
+            pers1.prenume = "Marian";
+            pers1.varsta = 30;
+
+            Console.WriteLine();
+            Console.WriteLine("Nume: " + pers1.nume + "\nPrenume: " + pers1.prenume + "\nVarsta: " + pers1.varsta);
+
+            Persoana pers2 = new Persoana("Popescu", "Ioana", 18);
+            Console.WriteLine();
+            Console.WriteLine("Nume: " + pers2.nume + "\nPrenume: " + pers2.prenume + "\nVarsta: " + pers2.varsta);
+
+            Console.ReadLine();
+        }
+    }
+}
